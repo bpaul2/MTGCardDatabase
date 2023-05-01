93f88975e82da39c3852978847abef262fecf493
bpaul2 <paulson2@ualberta.ca>
2023-04-30 23:07:24 -0600

    Update app functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Shapes;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MTGDB_Test
 {
@@ -25,18 +27,106 @@ namespace MTGDB_Test
     /// </summary>
     public partial class MainWindow : Window
     {
+        private APIClient client;
+        private Boolean firstSearch;
         public MainWindow()
         {
             InitializeComponent();
+            client = new APIClient("https://api.scryfall.com/");
+            firstSearch = true;
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Calls client to peform search
+        /// </summary>
+        private void performSearch()
         {
+            // Get card info
+            string name = SearchText.Text;
+            Card card = new Card();
+            card = client.SearchCard(name);
 
+            // Update window
+            CardInfo.Text = card.name;
+
+            // Update image
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(card.imgSource);
+            myBitmapImage.DecodePixelWidth = 146;
+            myBitmapImage.EndInit();
+            ImageBox.Source = myBitmapImage;
+        }
+        /// <summary>
+        /// Handles 'Get Card' button cick
+        /// </summary>
+        private void GetCardClick(object sender, RoutedEventArgs e)
+        {
+            performSearch();
+        }
+        /// <summary>
+        /// Handles search entry
+        /// </summary>
+        private void GetSearchEnter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                performSearch();
+            }   
+        }
+        /// <summary>
+        /// Handles clicking on 
+        /// </summary>
+        private void GetSearchClick(object sender, MouseButtonEventArgs e)
+        {
+            if (firstSearch)
+            {
+                SearchText.Text = "";
+                firstSearch = false;
+            }
         }
     }
-    private void GetCardClick(object sender, RoutedEventArgs e)
+    public class Card
     {
-        //
+        public string name { get; set; }
+        public string imgSource { get; set; }
+        public Card()
+        {
+            name = null;
+            imgSource = null;
+        }
+    }
+    public class APIClient
+    {
+        protected readonly string endpoint;
+        public APIClient(string endpoint) { this.endpoint = endpoint; }
+        
+        /// <summary>
+        /// Perform search query for singular card
+        /// </summary>
+        public Card SearchCard(string name)
+        {
+            string parameters = "cards/named?fuzzy=" + name;
+            JObject result = GetQuery(parameters);
+            Card card = new Card
+            {
+                name = (string)result["name"],
+                imgSource = (string)result["image_uris"]["small"]
+            };
+            return card;
+        }
+        /// <summary>
+        /// Perform query on API
+        /// </summary>
+        private JObject GetQuery(string parameters)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var query = endpoint + parameters;
+
+                var response = httpClient.GetAsync(query).Result;
+
+                return JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            }
+        }
     }
 }
