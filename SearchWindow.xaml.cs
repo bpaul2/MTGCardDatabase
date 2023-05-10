f617a562e42181d1b6013945beae58b19a578231
bpaul2 <paulson2@ualberta.ca>
2023-05-10 15:08:04 -0600

    Updated additional card text for single and double sided cards

--- SearchWindow.xaml.cs
+++ SearchWindow.xaml.cs
@@ -28,17 +28,20 @@ namespace MTGDB_Test
         private readonly APIClient _client;
         private bool _firstSearch;
         private readonly string _endpoint = "https://api.scryfall.com/";
+        private Card _card;
         // Layer ordering for image boxes
         private int _topImgOrder = 1;
         private int _botImgOrder = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             _client = new APIClient(_endpoint);
             _firstSearch = true;
+            _card = new Card();
         }
         /// <summary>
-        /// Calls client to peform search
+        /// Calls client to peform card search
         /// </summary>
         private void performSearch()
         {
@@ -46,44 +49,33 @@ namespace MTGDB_Test
             {
                 // Get card info
                 string name = SearchText.Text;
-                Card card = new Card();
-                card = _client.SearchCard(name);
+                _card = _client.SearchCard(name);
 
                 // Update window
-                CardInfo.Text = card.Name;
+                SetCardTxt(_card.DescFront());
 
-                // Update images
-                ImageBox1.Source = SetImage(card.ImgSource1);
-                ImageBox2.Source = SetImage(card.ImgSource2);
-
-                Console.WriteLine(ImageBox1.Source);
-                Console.WriteLine(ImageBox2.Source);
+                // Update image boxes
+                ImageBox1.Source = GetCardImage(_card.ImgSource1);
+                ImageBox2.Source = _card.DoubleSided ? GetCardImage(_card.ImgSource2) : null;
             }
             catch (FailedSearchException e)
             {
                 // Display error message
-                CardInfo.Text = e.Message;
+                SetCardTxt(e.Message);
                 // Reset image boxes
                 ImageBox1.Source = null;
                 ImageBox2.Source = null;
             }
         }
-        private BitmapImage SetImage(string uri)
+
+        private BitmapImage GetCardImage(string uri)
         {
-            if (uri != null)
-            {
-                BitmapImage myBitmapImage = new BitmapImage();
-                myBitmapImage.BeginInit();
-                myBitmapImage.UriSource = new Uri(uri);
-                myBitmapImage.DecodePixelWidth = 146;
-                myBitmapImage.EndInit();
-                return myBitmapImage;
-            }
-            else
-            {
-                Console.WriteLine("UhOh");
-                return null;
-            }
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(uri);
+            myBitmapImage.DecodePixelWidth = 146;
+            myBitmapImage.EndInit();
+            return myBitmapImage;
         }
         /// <summary>
         /// Handles 'Get Card' button cick
@@ -117,17 +109,24 @@ namespace MTGDB_Test
         private void MouseEnterImg1(object sender, MouseEventArgs e)
         {
             SwapImgOrdering(ImageBox1, ImageBox2);
+            SetCardTxt(_card.DescFront());
         }
 
         private void MouseEnterImg2(object sender, MouseEventArgs e)
         {
             SwapImgOrdering(ImageBox2, ImageBox1);
+            SetCardTxt(_card.DescBack());
         }
 
         private void SwapImgOrdering(Image imgTop, Image imgBot)
         {
             Panel.SetZIndex(imgTop, _topImgOrder);
             Panel.SetZIndex(imgBot, -_botImgOrder);
+        }
+
+        private void SetCardTxt(string txt)
+        {
+            CardInfo.Text = txt;
         }
     }
     public class FailedSearchException : Exception
@@ -139,13 +138,38 @@ namespace MTGDB_Test
     public class Card
     {
         public string Name { get; set; }
+        public string Set { get; set; }
+        public bool DoubleSided { get; set; }
+        public string SideName1 { get; set; }
+        public string SideName2 { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+        public string Flavor1 { get; set; }
+        public string Flavor2 { get; set; }
         public string ImgSource1 { get; set; }
         public string ImgSource2 { get; set; }
+
         public Card()
         {
-            Name = null;
             ImgSource1 = null;
             ImgSource2 = null;
+        }
+
+        public string DescFront()
+        {
+            return SideName1 + "\n" + Type1 + "\n" + Text1 + "\n" + Flavor1 + "\n";
+        }
+
+        public string DescBack()
+        {
+            return SideName2 + "\n" + Type2 + "\n" + Text2 + "\n" + Flavor2 + "\n";
+        }
+
+        public override string ToString()
+        {
+            return Name + "\n" + Set + "\n" + DescFront() + DescBack();
         }
     }
     public class APIClient
@@ -161,20 +185,37 @@ namespace MTGDB_Test
             string parameters = "cards/named?fuzzy=" + name;
             JObject result = GetQuery(parameters);
 
-            //Console.WriteLine(result.ToString());
+            Console.WriteLine(result.ToString());
 
             if ((string)result["object"] == "card")
             {
                 Card card = new Card();
                 card.Name = (string)result["name"];
+                card.Set = (string)result["set_name"];
                 // Check if card is double-sided
                 if (result.ContainsKey("card_faces"))
                 {
+                    card.DoubleSided = true;
+                    // Frontside
+                    card.SideName1 = (string)result["card_faces"][0]["name"];
+                    card.Type1 = (string)result["card_faces"][0]["type_line"];
+                    card.Text1 = (string)result["card_faces"][0]["oracle_text"];
+                    card.Flavor1 = (string)result["card_faces"][0]["flavor_text"];
                     card.ImgSource1 = (string)result["card_faces"][0]["image_uris"]["small"];
+                    // Backside
+                    card.SideName2 = (string)result["card_faces"][1]["name"];
+                    card.Type2 = (string)result["card_faces"][1]["type_line"];
+                    card.Text2 = (string)result["card_faces"][1]["oracle_text"];
+                    card.Flavor2 = (string)result["card_faces"][1]["flavor_text"];
                     card.ImgSource2 = (string)result["card_faces"][1]["image_uris"]["small"];
                 }
                 else
                 {
+                    card.DoubleSided = false;
+                    card.SideName1 = card.Name;
+                    card.Type1 = (string)result["type_line"];
+                    card.Text1 = (string)result["oracle_text"];
+                    card.Flavor1 = (string)result["flavor_text"];
                     card.ImgSource1 = (string)result["image_uris"]["small"];
                 }
 
